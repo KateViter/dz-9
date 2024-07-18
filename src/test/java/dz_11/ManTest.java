4aa89f8d3096289379fea31c74a019d9dc71ada9
pc-629 <schedrovaekaterina@gmail.com>
2024-07-18 12:12:50 +0300

    tests for man and woman classes

--- src/test/java/dz_11/ManTest.java
+++ src/test/java/dz_11/ManTest.java
@@ -5,47 +5,58 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ManTest {
+    private static final String manName = "Sergey";
+    private static final String manLastName = "Lukyanenko";
+    private static final String manPartner = "Luiza";
+    private static final int manAge = 37;
+
+    private static final int retirementAge = 66;
+    private static final String ChangedName = "Ivan";
+    private static final String ChangedLastName = "Ivanov";
+    private static final String ChangedPartner = "Natalia";
+
     @Test(testName = "test constructor")
     public void testConstructor() {
-        Man man = new Man("Sergey", "Lukyanenko", 37, "Luiza");
-        Assert.assertEquals(man.getFirstName(), "Sergey", "first name is not valid");
-        Assert.assertEquals(man.getLastName(), "Lukyanenko", "last name is not valid");
-        Assert.assertEquals(man.getAge(), 37, "age is not valid");
-        Assert.assertEquals(man.getPartner(), "Luiza", "partner is not valid");
+        Man man = new Man(manName, manLastName, manAge, manPartner);
+        Assert.assertEquals(man.getFirstName(), manName, "first name is not valid");
+        Assert.assertEquals(man.getLastName(), manLastName, "last name is not valid");
+        Assert.assertEquals(man.getAge(), manAge, "age is not valid");
+        Assert.assertEquals(man.getPartner(), manPartner, "partner is not valid");
     }
     @Test(testName = "test retirement")
     public void testRetirement()  {
-        Man man = new Man("Sergey", "Lukyanenko", 66, "Luiza");
-        Assert.assertTrue(man.isRetired(), "wrong calculated retirement when old");
-        man.setAge(65);
+        Man man = new Man(manName, manLastName, manAge, manPartner);
         Assert.assertFalse(man.isRetired(), "wrong calculated retirement when young");
+        man.setAge(retirementAge);
+        Assert.assertTrue(man.isRetired(), "wrong calculated retirement when old");
+
     }
 
     @Test(testName = "test first name", groups = "setters")
     public void testSetterFirstName() {
-        Man man = new Man("Sergey", "Lukyanenko", 37, "Luiza");
-        man.setFirstName("Ivan");
-        Assert.assertEquals(man.getFirstName(), "Ivan", "first name is not valid");
+        Man man = new Man(manName, manLastName, manAge, manPartner);
+        man.setFirstName(ChangedName);
+        Assert.assertEquals(man.getFirstName(), ChangedName, "first name is not valid");
     }
 
     @Test(testName = "test last name", groups = "setters")
     public void testSetterLastName() {
-        Man man = new Man("Sergey", "Lukyanenko", 37, "Luiza");
-        man.setLastName("Ivanov");
-        Assert.assertEquals(man.getLastName(), "Ivanov", "last name is not valid");
+        Man man = new Man(manName, manLastName, manAge, manPartner);
+        man.setLastName(ChangedLastName);
+        Assert.assertEquals(man.getLastName(), ChangedLastName, "last name is not valid");
     }
 
     @Test(testName = "test age", groups = "setters")
     public void testSetterAge() {
-        Man man = new Man("Sergey", "Lukyanenko", 37, "Luiza");
-        man.setAge(38);
-        Assert.assertEquals(man.getAge(), 38, "age is not valid");
+        Man man = new Man(manName, manLastName, manAge, manPartner);
+        man.setAge(retirementAge);
+        Assert.assertEquals(man.getAge(), retirementAge, "age is not valid");
     }
 
     @Test(testName = "test partner", groups = "setters")
     public void testSetterPartner() {
-        Man man = new Man("Sergey", "Lukyanenko", 37, "Luiza");
-        man.setPartner("Liza Ivanova");
-        Assert.assertEquals(man.getPartner(), "Liza Ivanova", "partner is not valid");
+        Man man = new Man(manName, manLastName, manAge, manPartner);
+        man.setPartner(ChangedPartner);
+        Assert.assertEquals(man.getPartner(), ChangedPartner, "partner is not valid");
     }
 }
--- src/test/java/dz_11/WomanTest.java
+++ src/test/java/dz_11/WomanTest.java
@@ -5,63 +5,74 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WomanTest {
+    private static final String womanName = "Agni";
+    private static final String womanLastName = "Barto";
+    private static final String womanPartner = "Kolin";
+    private static final int womanAge = 19;
+
+    private static final int retirementAge = 61;
+    private static final String ChangedName = "Sofi";
+    private static final String ChangedLastName = "Lauren";
+    private static final String ChangedPartner = "Stefan";
+    private static final String partnerLastName = "Bridgerton";
+
     @Test(testName = "test constructor")
     public void testConstructor() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        Assert.assertEquals(woman.getFirstName(), "Agni", "first name is not valid");
-        Assert.assertEquals(woman.getLastName(), "Barto", "last name is not valid");
-        Assert.assertEquals(woman.getAge(), 19, "age is not valid");
-        Assert.assertEquals(woman.getPartner(), "Kolin", "partner is not valid");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        Assert.assertEquals(woman.getFirstName(), womanName, "first name is not valid");
+        Assert.assertEquals(woman.getLastName(), womanLastName, "last name is not valid");
+        Assert.assertEquals(woman.getAge(), womanAge, "age is not valid");
+        Assert.assertEquals(woman.getPartner(), womanPartner, "partner is not valid");
     }
     @Test(testName = "test retirement")
     public void testRetirement()  {
-        Woman woman = new Woman("Agni", "Barto", 61, "Kolin");
-        Assert.assertTrue(woman.isRetired(), "wrong calculated retirement when old");
-        woman.setAge(60);
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
         Assert.assertFalse(woman.isRetired(), "wrong calculated retirement when young");
+        woman.setAge(retirementAge);
+        Assert.assertTrue(woman.isRetired(), "wrong calculated retirement when old");
     }
 
     @Test()
     public void testRegisterPartnership() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.registerPartnership("Bridgerton");
-        Assert.assertEquals(woman.getLastName(), "Bridgerton", "wrong last name after registration");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.registerPartnership(partnerLastName);
+        Assert.assertEquals(woman.getLastName(), partnerLastName, "wrong last name after registration");
     }
 
     @Test(testName = "test deregisterPartnership")
     public void testDeregisterPartnership() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.registerPartnership("Bridgerton");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.registerPartnership(partnerLastName);
         woman.deregisterPartnership(true);
-        Assert.assertEquals(woman.getLastName(), "Barto", "wrong last name after deregistration");
+        Assert.assertEquals(woman.getLastName(), womanLastName, "wrong last name after deregistration");
     }
 
     @Test(testName = "test first name", groups = "setters")
     public void testSetterFirstName() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.setFirstName("Sofi");
-        Assert.assertEquals(woman.getFirstName(), "Sofi", "first name is not valid");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.setFirstName(ChangedName);
+        Assert.assertEquals(woman.getFirstName(), ChangedName, "first name is not valid");
     }
 
     @Test(testName = "test last name", groups = "setters")
     public void testSetterLastName() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.setLastName("Lauren");
-        Assert.assertEquals(woman.getLastName(), "Lauren", "last name is not valid");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.setLastName(ChangedLastName);
+        Assert.assertEquals(woman.getLastName(), ChangedLastName, "last name is not valid");
     }
 
     @Test(testName = "test age", groups = "setters")
     public void testSetterAge() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.setAge(21);
-        Assert.assertEquals(woman.getAge(), 21, "age is not valid");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.setAge(retirementAge);
+        Assert.assertEquals(woman.getAge(), retirementAge, "age is not valid");
     }
 
     @Test(testName = "test partner", groups = "setters")
     public void testSetterPartner() {
-        Woman woman = new Woman("Agni", "Barto", 19, "Kolin");
-        woman.setPartner("Stefan");
-        Assert.assertEquals(woman.getPartner(), "Stefan", "partner is not valid");
+        Woman woman = new Woman(womanName, womanLastName, womanAge, womanPartner);
+        woman.setPartner(ChangedPartner);
+        Assert.assertEquals(woman.getPartner(), ChangedPartner, "partner is not valid");
     }
 
 }
