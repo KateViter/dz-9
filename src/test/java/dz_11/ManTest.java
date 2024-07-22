9f1f891808fc235474385d3f70c6d43f69642bf6
pc-629 <schedrovaekaterina@gmail.com>
2024-07-22 19:49:16 +0300

    dataDriven tests update

--- src/test/java/dz_11/ManTest.java
+++ src/test/java/dz_11/ManTest.java
@@ -2,29 +2,34 @@ package dz_11;
 
 import org.example.Man;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ManTest {
-    private static final String manName = "Sergey";
-    private static final String manLastName = "Lukyanenko";
-    private static final String manPartner = "Luiza";
-    private static final int manAge = 37;
 
-    private static final int retirementAge = 66;
-    private static final String ChangedName = "Ivan";
-    private static final String ChangedLastName = "Ivanov";
-    private static final String ChangedPartner = "Natalia";
+    @DataProvider(name = "inputData")
+    public Object[][] manData() {
+        return new Object[][]{
+                {"Sergey","Lukyanenko","Luiza",37,"Ivan","Ivanov","Natalia",66},
+                {"Paul","Kalkbrenner","Marie",42,"Roy","Belinskiy","Gorgia",99},
+                {"Kristopher","Paoilini","Diana",53,"Stefan","White","Mi",112}
+        };
+    }
 
-    @Test(testName = "test constructor")
-    public void testConstructor() {
+    @Test(testName = "test constructor",dataProvider = "inputData")
+    public void testConstructor(String manName, String manLastName, String manPartner, int manAge,
+                                String ignoredChangedName, String ignoredChangedLastName,
+                                String ignoredChangedPartner, int ignoredRetirementAge) {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         Assert.assertEquals(man.getFirstName(), manName, "first name is not valid");
         Assert.assertEquals(man.getLastName(), manLastName, "last name is not valid");
         Assert.assertEquals(man.getAge(), manAge, "age is not valid");
         Assert.assertEquals(man.getPartner(), manPartner, "partner is not valid");
     }
-    @Test(testName = "test retirement")
-    public void testRetirement()  {
+    @Test(testName = "test retirement", dataProvider = "inputData")
+    public void testRetirement(String manName, String manLastName, String manPartner, int manAge,
+                               String ignoredChangedName, String ignoredChangedLastName,
+                               String ignoredChangedPartner, int retirementAge)  {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         Assert.assertFalse(man.isRetired(), "wrong calculated retirement when young");
         man.setAge(retirementAge);
@@ -32,29 +37,37 @@ public class ManTest {
 
     }
 
-    @Test(testName = "test first name", groups = "setters")
-    public void testSetterFirstName() {
+    @Test(testName = "test first name", groups = "setters", dataProvider = "inputData")
+    public void testSetterFirstName(String manName, String manLastName, String manPartner, int manAge,
+                                    String ChangedName, String ignoredChangedLastName,
+                                    String ignoredChangedPartner, int ignoredRetirementAge) {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         man.setFirstName(ChangedName);
         Assert.assertEquals(man.getFirstName(), ChangedName, "first name is not valid");
     }
 
-    @Test(testName = "test last name", groups = "setters")
-    public void testSetterLastName() {
+    @Test(testName = "test last name", groups = "setters", dataProvider = "inputData")
+    public void testSetterLastName(String manName, String manLastName, String manPartner, int manAge,
+                                   String ignoredChangedName, String ChangedLastName,
+                                   String ignoredChangedPartner, int ignoredRetirementAge) {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         man.setLastName(ChangedLastName);
         Assert.assertEquals(man.getLastName(), ChangedLastName, "last name is not valid");
     }
 
-    @Test(testName = "test age", groups = "setters")
-    public void testSetterAge() {
+    @Test(testName = "test age", groups = "setters", dataProvider = "inputData")
+    public void testSetterAge(String manName, String manLastName, String manPartner, int manAge,
+                              String ignoredChangedName, String ignoredChangedLastName,
+                              String ignoredChangedPartner, int retirementAge) {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         man.setAge(retirementAge);
         Assert.assertEquals(man.getAge(), retirementAge, "age is not valid");
     }
 
-    @Test(testName = "test partner", groups = "setters")
-    public void testSetterPartner() {
+    @Test(testName = "test partner", groups = "setters", dataProvider = "inputData")
+    public void testSetterPartner(String manName, String manLastName, String manPartner, int manAge,
+                                  String ignoredChangedName, String ignoredChangedLastName,
+                                  String ChangedPartner, int ignoredRetirementAge) {
         Man man = new Man(manName, manLastName, manAge, manPartner);
         man.setPartner(ChangedPartner);
         Assert.assertEquals(man.getPartner(), ChangedPartner, "partner is not valid");
